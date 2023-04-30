57bfba8df3f4d290d740543b0e89848d90da8e79
CodesTVP <techdragonarray@gmail.com>
2023-04-30 10:41:31 -0300

    💹 Statistics

---
+++ min/statisticsAds-min.js
@@ -0,0 +1,10 @@
+const ads=document.querySelectorAll('.ad')
+const observer=new IntersectionObserver((entries)=>{entries.forEach((entry)=>{const id=entry.target.id
+if(entry.isIntersecting)
+setTimeout(()=>{if(entry.isIntersecting)
+fetch(`https://serve-ads.onrender.com/post?type=views&id=${id}`,{method:'POST'})},2000)
+else clearTimeout()})})
+ads.forEach(ad=>{observer.observe(ad);fetch(`https://serve-ads.onrender.com/post?type=prints&id=${id}`,{method:'POST'})
+ad.onclick=e=>{const id=ad.id
+console.log(e)
+fetch(`https://serve-ads.onrender.com/post?type=clicks&id=${id}`,{method:'POST'})}})
